5a321723320f0df4f87f72c036daf8fdc8e15446
Ethan <ethan@Ethans-MacBook-Pro.local>
2013-12-01 15:41:37 -0600

    Initial Commit

---
+++ Quiz/QuizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuizAppDelegate.h
+//  Quiz
+//
+//  Created by Ethan on 12/1/13.
+//  Copyright (c) 2013 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Ethan on 12/1/13.
+//  Copyright (c) 2013 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
